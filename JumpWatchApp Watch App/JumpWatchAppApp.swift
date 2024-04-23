67dd6261376e0a2c05958a6ce6a24e1c7bd0171c
HarnoorSethi <sethinoor@gmail.com>
2024-04-23 10:21:51 -0700

    Added Watch Target Still need to fix zero velocity glitch Clean up animation Add score Watch does not work at all; needs to be fixed

---
+++ JumpWatchApp Watch App/JumpWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumpWatchAppApp.swift
+//  JumpWatchApp Watch App
+//
+//  Created by Harnoor Sethi on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct JumpWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
